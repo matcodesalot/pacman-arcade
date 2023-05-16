023fc61b39f3587576a8a6f58a6bff95ece179cb
matcodesalot <reallymathew@gmail.com>
2023-05-15 19:17:16 -0500

    Add pacman spawn point and hollow walls

--- main.js
+++ main.js
@@ -6,63 +6,73 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 //Define the 3D map that represents the scene
-//1: boundary, 0: empty space
+//1: boundary, 0: empty space, 2: player spawn point
 const map = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-    [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
 let score = 0;
+
+function createRect(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
+
+function createRectOutline(x, y, width, height, color) {
+    ctx.strokeStyle = color;
+    ctx.strokeRect(x, y, width, height);
+}
+
 
 
 class Boundary {
     static width = 40;
     static height = 40;
-    constructor({position}) {
+    constructor({position, width, height, color}) {
         this.position = position;
-        this.width = 40;
-        this.height = 40;
+        this.width = width;
+        this.height = height;
+        this.color = color;
     }
 
     draw() {
-        ctx.fillStyle = `#1a1ac7`;
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        createRect(this.position.x, this.position.y, this.width, this.height, this.color);
     }
 }
 
 class Player {
     constructor({position, velocity}) {
-        this.position = position;
-        this.velocity = velocity;
+        this.position = position || {x: 0, y: 0};
+        this.velocity = velocity || {x: 0, y: 0};
         this.radius = 15;
         this.speed = 3;
     }
@@ -79,6 +89,13 @@ class Player {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
+
+        if(this.position.x <= 0) {
+            console.log("we out this piece");
+        }
+        if(this.position.x >= canvas.width) {
+            console.log("we also out this piece");
+        }
     }
 }
 
@@ -90,23 +107,13 @@ class Food {
     }
 
     draw() {
-        ctx.fillStyle = `white`;
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        createRect(this.position.x, this.position.y, this.width, this.height, `white`);
     }
 }
 
 const boundaries = [];
 const foods = [];
-const player = new Player({
-    position: {
-        x: Boundary.width + Boundary.width / 2,
-        y: Boundary.height + Boundary.height / 2
-    },
-    velocity: {
-        x: 0,
-        y: 0
-    }
-});
+const player = new Player({});
 
 const keys = {
     up: {
@@ -125,28 +132,103 @@ const keys = {
 
 let lastKey = ``;
 
-map.forEach((row, i) => {
-    row.forEach((symbol, j) => {
-        switch(symbol) {
-            case 1:
+const oneBlockSize = 40;
+const wallColor = `#1a1ac7`;
+const wallInnerColor = `black`;
+const wallSpaceWidth = oneBlockSize / 1.2;
+const wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+
+
+//iterate over the map
+for (let row = 0; row < map.length; row++) {
+    for (let col = 0; col < map[row].length; col++) {
+        const symbol = map[row][col];
+
+        //Check if the current cell contains a 1
+        if (symbol === 1) {
+            boundaries.push(new Boundary({
+                position: {
+                    x: col * Boundary.width,
+                    y: row * Boundary.height
+                },
+                width: Boundary.width,
+                height: Boundary.height,
+                color: wallColor
+            }));
+
+            //Check if neighboring cells are also walls
+            const topCell = row > 0 && map[row - 1][col] === 1;
+            const bottomCell = row < map.length - 1 && map[row + 1][col] === 1;
+            const leftCell = col > 0 && map[row][col - 1] === 1;
+            const rightCell = col < map[row].length - 1 && map[row][col + 1] === 1;
+
+            if(topCell) {
                 boundaries.push(new Boundary({
                     position: {
-                        x: Boundary.width * j,
-                        y: Boundary.height * i
-                    }
+                        x: col * Boundary.width + wallOffset,
+                        y: row * Boundary.height
+                    },
+                    width: Boundary.width / 1.2,
+                    height: Boundary.height / 1.2 + wallOffset,
+                    color: wallInnerColor
                 }));
-                break;
-            case 0:
-                foods.push(new Food({
+            }
+
+            if(bottomCell) {
+                boundaries.push(new Boundary({
                     position: {
-                        x: Boundary.width * j + Boundary.width / 2,
-                        y: Boundary.height * i + Boundary.height / 2
-                    }
+                        x: col * Boundary.width + wallOffset,
+                        y: row * Boundary.height + wallOffset
+                    },
+                    width: Boundary.width / 1.2,
+                    height: Boundary.height / 1.2 + wallOffset,
+                    color: wallInnerColor
                 }));
-                break;
+            }
+
+            if(leftCell) {
+                boundaries.push(new Boundary({
+                    position: {
+                        x: col * Boundary.width,
+                        y: row * Boundary.height + wallOffset
+                    },
+                    width: Boundary.width / 1.2 + wallOffset,
+                    height: Boundary.height / 1.2,
+                    color: wallInnerColor
+                }));
+            }
+
+            if(rightCell) {
+                boundaries.push(new Boundary({
+                    position: {
+                        x: col * Boundary.width + wallOffset,
+                        y: row * Boundary.height + wallOffset
+                    },
+                    width: Boundary.width / 1.2 + wallOffset,
+                    height: Boundary.height / 1.2,
+                    color: wallInnerColor
+                }));
+            }
         }
-    });
-});
+
+        //Check if the current cell contains a 2
+        if (symbol === 2) {
+            player.position.x = col * Boundary.width + Boundary.width / 2;
+            player.position.y = row * Boundary.height + Boundary.height / 2;
+        }
+
+        //Check if the current cell contains a 0
+        if (symbol === 0) {
+            foods.push(new Food({
+                position: {
+                    x: col * Boundary.width + Boundary.width / 2,
+                    y: row * Boundary.height + Boundary.height / 2
+                }
+            }));
+        }
+    }
+}
+
 
 
 window.addEventListener(`keydown`, (e) => {
@@ -154,22 +236,22 @@ window.addEventListener(`keydown`, (e) => {
         case `ArrowUp`:
         case `w`:
             keys.up.pressed = true;
-            lastKey = `ArrowUp`;
+            lastKey = `up`;
             break;
         case `ArrowDown`:
         case `s`:
             keys.down.pressed = true;
-            lastKey = `ArrowDown`;
+            lastKey = `down`;
             break;
         case `ArrowLeft`:
         case `a`:
             keys.left.pressed = true;
-            lastKey = `ArrowLeft`;
+            lastKey = `left`;
             break;
         case `ArrowRight`:
         case `d`:
             keys.right.pressed = true;
-            lastKey = `ArrowRight`;
+            lastKey = `right`;
             break;
     }
 });
@@ -209,7 +291,7 @@ function gameLoop() {
     //Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if(keys.up.pressed && lastKey === `ArrowUp`) {
+    if(keys.up.pressed && lastKey === `up`) {
         for(let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i];
             if(circleCollidesWithRectangle({
@@ -227,7 +309,7 @@ function gameLoop() {
             }
         }
     }
-    if(keys.down.pressed && lastKey === `ArrowDown`) {
+    if(keys.down.pressed && lastKey === `down`) {
         for(let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i];
             if(circleCollidesWithRectangle({
@@ -245,7 +327,7 @@ function gameLoop() {
             }
         }
     }
-    if(keys.left.pressed && lastKey === `ArrowLeft`) {
+    if(keys.left.pressed && lastKey === `left`) {
         for(let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i];
             if(circleCollidesWithRectangle({
@@ -263,7 +345,7 @@ function gameLoop() {
             }
         }
     }
-    if(keys.right.pressed && lastKey === `ArrowRight`) {
+    if(keys.right.pressed && lastKey === `right`) {
         for(let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i];
             if(circleCollidesWithRectangle({
@@ -290,7 +372,7 @@ function gameLoop() {
             boundary: food
         })) {
             foods.splice(i, 1);
-            console.log(score++);
+            //console.log(score++);
         }
     });
 
